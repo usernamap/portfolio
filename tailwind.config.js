c8cc3a3e912c404460ea35e369ecca41c6efcb53
usernamap <lucas.blanchard@epitech.eu>
2023-06-22 21:16:11 +0200

    delete and rebuild flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html",  "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+]
 }
 
