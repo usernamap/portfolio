830d047ac5a36a6d5dacd7160d5c221f76c0474d
usernamap <lucas.blanchard@epitech.eu>
2023-06-23 13:43:37 +0200

    re-add worker

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,33 @@
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('v2').then(function(cache) {
-        return cache.addAll([
-          'css/style.css',
-          'css/darkmode.css',
-          'css/responsive.css',
-          'css/animation.css',
-          'css/construct_page.css',
-          'js/animations.js',
-          'js/construct_page.js',
-          'js/darkmode.js',
-          'js/main.js',
-          '/index.html',
-          '/'
-        ]);
+  event.waitUntil(
+    caches.open('v2').then(function(cache) {
+      return cache.addAll([
+        'css/style.css',
+        'css/darkmode.css',
+        'css/responsive.css',
+        'css/animation.css',
+        'css/construct_page.css',
+        'js/animations.js',
+        'js/construct_page.js',
+        'js/darkmode.js',
+        'js/main.js',
+        '/index.html',
+        '/'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('v2').then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        var fetchPromise = fetch(event.request).then(function(networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        })
+        return response || fetchPromise;
       })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        if (response !== undefined) {
-          return response;
-        } else {
-          return fetch(event.request).then(function (response) {
-            let responseClone = response.clone();
-            
-            caches.open('v2').then(function (cache) {
-              cache.put(event.request, responseClone);
-            });
-  
-            return response;
-          }).catch(function () {
-            return caches.match('/index.html');
-          });
-        }
-      })
-    );
-  });
-  
+    })
+  );
+});
